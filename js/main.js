2db2d9d64cd019480dc1eae4e0823ceeafc3a6d9
snowIceg <gxbkss@163.com>
2019-12-24 14:38:25 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,35 @@
+//
+// let myHeading = document.querySelector('h1');
+// myHeading.textContent = 'Hello world!';
+
+let myImage=document.querySelector('img');
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+myImage.onclick=function () {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/firefox-icon.png') {
+        myImage.setAttribute('src', 'images/firefox2.png');
+    } else {
+        myImage.setAttribute('src', 'images/firefox-icon.png');
+    }
+}
+
+function setUserName() {
+    let myName = prompt('请输入你的名字。');
+    if(!myName || myName === null) {
+        setUserName();
+    } else {
+        localStorage.setItem('name', myName);
+        myHeading.innerHTML = 'Mozilla 酷毙了，' + myName;
+    }
+}
+
+if(!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla 酷毙了，' + storedName;
+}
+myButton.onclick = function() {
+    setUserName();
+}
